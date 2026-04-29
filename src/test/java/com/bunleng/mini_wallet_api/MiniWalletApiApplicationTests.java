6e383f167c7e50a6c58dc892192e530a25ec8389
DrRaspec <leng79867@gmail.com>
2026-04-29 13:59:14 +0700

    Add src/test/

---
+++ src/test/java/com/bunleng/mini_wallet_api/MiniWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunleng.mini_wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWalletApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
