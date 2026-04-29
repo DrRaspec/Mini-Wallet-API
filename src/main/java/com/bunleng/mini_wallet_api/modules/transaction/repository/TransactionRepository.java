5bf694b2c829e9ba9323246df5e733146db91352
DrRaspec <leng79867@gmail.com>
2026-04-29 16:49:55 +0700

    fix(transaction-repository): correct query account balance

--- src/main/java/com/bunleng/mini_wallet_api/modules/transaction/repository/TransactionRepository.java
+++ src/main/java/com/bunleng/mini_wallet_api/modules/transaction/repository/TransactionRepository.java
@@ -4,15 +4,19 @@ import com.bunleng.mini_wallet_api.modules.transaction.dto.BalanceResponse;
 import com.bunleng.mini_wallet_api.modules.transaction.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     @Query("""
-        SELECT
-            SUM(CASE WHEN t.isIncome = true THEN t.amount ELSE 0 END),
-            SUM(CASE WHEN t.isIncome = false THEN t.amount ELSE 0 END)
-        FROM Transaction t
-    """)
+    SELECT new com.bunleng.mini_wallet_api.modules.transaction.dto.BalanceResponse(
+        null,
+        SUM(CASE WHEN t.isIncome = true THEN t.amount ELSE 0.0 END),
+        SUM(CASE WHEN t.isIncome = false THEN t.amount ELSE 0.0 END)
+    )
+    FROM Transaction t
+""")
     BalanceResponse getBalances();
 }
